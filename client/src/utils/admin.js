518f027be28742b80fadbeca2163448818fbbc97
FutureSea <futuresea.dev@gmail.com>
2024-11-10 04:06:57 +0300

    update ssl

--- client/src/utils/admin.js
+++ client/src/utils/admin.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // const token = useSelector((state) => state.auth.token);
 
 let api = axios.create({
-    baseURL: 'https://localhost:8000',
+    baseURL: 'http://localhost:8000',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let api = axios.create({
-    baseURL: 'https://localhost:8000',
+    baseURL: 'http://localhost:8000',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
